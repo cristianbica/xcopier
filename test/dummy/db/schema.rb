9dac4fd4c020354995fb177a7927b94cd381f9ef
Cristian Bica <cristian.bica@gmail.com>
2024-12-28 21:28:23 +0000

    CI Fix migration version

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 20_241_224_111_841) do
+ActiveRecord::Schema[7.0].define(version: 20_241_224_111_841) do
   create_table "companies", force: :cascade do |t|
     t.string "name"
     t.string "address"
